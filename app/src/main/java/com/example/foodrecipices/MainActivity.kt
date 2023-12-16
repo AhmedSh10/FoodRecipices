6d54fff0fdb8e3ffdde160defb03d820e9033e38
AhmedSh10 <ahmed.sherif3400@gmail.com>
2023-12-16 08:21:54 +0200

    food

--- app/src/main/java/com/example/foodrecipices/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipices/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.foodrecipices
 
+<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+=======
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+>>>>>>> ac6fa34 (food)
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+<<<<<<< HEAD
+=======
+     val bottomNavigation=findViewById<BottomNavigationView>(R.id.bottom_navigation)
+     val navController =Navigation.findNavController(this,R.id.frag_host)
+     NavigationUI.setupWithNavController(bottomNavigation,navController)
+
+
+
+
+>>>>>>> ac6fa34 (food)
     }
 }
